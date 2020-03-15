987fbfee20e6bb7defa69108d07cd7e6b4cdb442
Chanh Le <chanh@wonolo.com>
2020-03-15 20:46:07 +0700

    fix css

--- app/javascript/custom/main.js
+++ app/javascript/custom/main.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).on('turbolinks:load', function() {
   $(".add-new-question").click(function() {
     $(".add-question-btn a").click();
     $(".question-missing-error").html('')
